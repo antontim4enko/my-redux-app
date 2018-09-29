b596bb2f7a26cb1ed31cf2c27626471b294e8f16
antontim4enko <anton.tim4enko.2014@gmail.com>
2018-09-29 12:38:52 +0300

    name deleted

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { createStore } from 'redux';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const name = "Vassya";
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
